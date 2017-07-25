5df7e6381a497217d53a00e270e0c245ddcd9b7c
Jerd <inquisitivequin@gmail.com>
2017-07-24 19:44:56 -0700

    funcs

--- scrpt.js
+++ scrpt.js
@@ -1,4 +1,3 @@
-"use strict";
 var nom = 'Poat';
 var age = 32;
 var hobs = ['food', 'moreFood'];
@@ -10,3 +9,18 @@ var Veg;
 })(Veg || (Veg = {}));
 var myVeg = Veg.onion;
 console.log(myVeg);
+var car = "audi";
+console.log(car);
+car = { brand: 'audi', year: 2001 };
+console.log(car.year);
+function returnMe() {
+    return nom;
+}
+console.log(returnMe());
+function muli(val2, val1) {
+    return val2 * val1;
+}
+console.log(muli(12, 12));
+var mo;
+mo = returnMe;
+console.log(mo(10, 10));
--- scrpt.ts
+++ scrpt.ts
@@ -13,3 +13,31 @@ enum Veg {
 let myVeg: Veg = Veg.onion
 
 console.log(myVeg)
+
+let car: any = "audi"
+
+console.log(car)
+
+car = { brand: 'audi', year: 2001}
+
+console.log(car.year)
+
+//funcs
+
+function returnMe(): string {
+	return nom
+}
+
+console.log(returnMe())
+
+function muli(val2: number, val1: number): number {
+	return val2 * val1
+}
+
+console.log(muli(12,12))
+
+let mo;
+
+mo = returnMe
+
+console.log(mo(10,10))
