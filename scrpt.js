1781b99c8993293c4c1ff4f7c3afc8c1319f0b97
Jerd <inquisitivequin@gmail.com>
2017-07-24 18:56:35 -0700

    tings

--- scrpt.js
+++ scrpt.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('test');
+var nom = 'Poat';
+var age = 32;
+var hobs = ['food', 'moreFood'];
+var Veg;
+(function (Veg) {
+    Veg[Veg["tomato"] = 0] = "tomato";
+    Veg[Veg["onion"] = 1] = "onion";
+    Veg[Veg["potato"] = 2] = "potato";
+})(Veg || (Veg = {}));
+var myVeg = Veg.onion;
+console.log(myVeg);
--- scrpt.ts
+++ scrpt.ts
@@ -1,1 +1,15 @@
-console.log('test')
+let nom = 'Poat'
+
+let age = 32
+
+let hobs = ['food', 'moreFood']
+
+enum Veg {
+	'tomato',
+	'onion',
+	'potato'
+}
+
+let myVeg: Veg = Veg.onion
+
+console.log(myVeg)
