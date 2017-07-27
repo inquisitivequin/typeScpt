06be8a0413a7757a397119c21eb27e813eb45b12
Jerd <inquisitivequin@gmail.com>
2017-07-26 18:40:29 -0700

    scrpt

--- scrpt.js
+++ scrpt.js
@@ -1,30 +1,7 @@
-var nom = 'Poat';
-var age = 32;
-var hobs = ['food', 'moreFood'];
-var Veg;
-(function (Veg) {
-    Veg[Veg["tomato"] = 0] = "tomato";
-    Veg[Veg["onion"] = 1] = "onion";
-    Veg[Veg["potato"] = 2] = "potato";
-})(Veg || (Veg = {}));
-var myVeg = Veg.onion;
-console.log(myVeg);
-var car = "audi";
-console.log(car);
-car = { brand: 'audi', year: 2001 };
-console.log(car.year);
-function returnMe() {
-    return nom;
-}
-console.log(returnMe());
-function muli(val2, val1) {
-    return val2 * val1;
-}
-console.log(muli(12, 12));
-var mo;
-mo = returnMe;
-var ustr = {
-    name: 'Poat',
-    age: 1324
+var _this = this;
+var bankAccount = {
+    money: 2000,
+    depsoit: function (val) {
+        return _this.money += val;
+    }
 };
-console.log(ustr);
--- scrpt.ts
+++ scrpt.ts
@@ -1,51 +1,16 @@
-let nom = 'Poat'
-
-let age = 32
-
-let hobs = ['food', 'moreFood']
-
-enum Veg {
-	'tomato',
-	'onion',
-	'potato'
+let bankAccount: {money: number, deposit: (val:number) => val} = {
+	money: 2000,
+	depsoit: (val:number) => {
+		return this.money += val
+	}
 }
 
-let myVeg: Veg = Veg.onion
-
-console.log(myVeg)
-
-let car: any = "audi"
-
-console.log(car)
-
-car = { brand: 'audi', year: 2001}
-
-console.log(car.year)
-
-//funcs
-
-function returnMe(): string {
-	return nom
+let myseld = {
+	name: "Poat",
+	bankAccnt: bankAccount,
+	hobs: ['food', 'more food']
 }
 
-console.log(returnMe())
+myseld.bankAccnt.depsoit(2)
 
-function muli(val2: number, val1: number): number {
-	return val2 * val1
-}
-
-console.log(muli(12,12))
-
-let mo;
-
-mo = returnMe
-
-
-//objs
-
-let ustr: { name: string, age: number } = {
-	name: 'Poat',
-	age: 1324
-}
-
-console.log(ustr)
+console.log(myseld)
