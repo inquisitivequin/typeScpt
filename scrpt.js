3fe779c775eb52c80232c961e61182d551a6e52b
Jerd <inquisitivequin@gmail.com>
2017-07-28 16:06:12 -0700

    restSpread

--- scrpt.js
+++ scrpt.js
@@ -1,1 +1,8 @@
+var t = 'test';
+console.log(t);
+t = "another test";
+console.log(t);
+console.log("Rest & Spread");
+var ar = [1, 2, 3, 4, 4, 423, 4];
+console.log(Math.max.apply(Math, ar));
 //# sourceMappingURL=scrpt.js.map
---
+++ scrpt.ts
@@ -0,0 +1,13 @@
+let t: string = 'test'
+
+console.log(t)
+
+t = "another test"
+
+console.log(t)
+
+console.log("Rest & Spread")
+
+const ar = [1,2,3,4,4,423,4]
+
+console.log(Math.max(...ar))
