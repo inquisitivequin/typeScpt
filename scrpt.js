5b38486d4fa7c14ce70d19b7d91080a329ef482f
Jerd <inquisitivequin@gmail.com>
2017-07-28 10:37:45 -0700

    sMp

--- scrpt.js
+++ scrpt.js
@@ -1,17 +1,1 @@
-var bankAccount = {
-    money: 2000,
-    deposit: function (val) {
-        return this.money;
-    }
-};
-var myseld = {
-    name: "Poat",
-    bankAccnt: bankAccount,
-    hobs: ['food', 'more food']
-};
-myseld.bankAccnt.deposit(2);
-console.log(myseld);
-function ply(val1, val2) {
-    return val1 * val2;
-}
-console.log(ply(12, 7));
+//# sourceMappingURL=scrpt.js.map
