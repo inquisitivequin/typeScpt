2c336cf4c233b142f75ac977ed856b844ad85b64
Jerd <inquisitivequin@gmail.com>
2017-07-29 12:24:36 -0700

    privected

--- scrpt.js
+++ scrpt.js
@@ -2,9 +2,20 @@ var Person = (function () {
     function Person(name, usrname) {
         if (usrname === void 0) { usrname = 'shea'; }
         this.usrname = usrname;
+        this.color = 'brown';
         this.name = name;
     }
+    Person.prototype.getColor = function () {
+        console.log(this.color);
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
-console.log(new Person('poat', 'jurd'));
+var poat = new Person('Shea', 'Poat');
+console.log(poat.name, poat.usrname);
+poat.getColor();
+poat.setType('Puppy');
 //# sourceMappingURL=scrpt.js.map
--- scrpt.ts
+++ scrpt.ts
@@ -4,11 +4,23 @@ class Person {
 	private type: string
 	//only accessible within block scope as well as any class or 
 	//objects that inherit from this class
-	protected color: string
+	protected color: string = 'brown'
 
 	constructor(name: string, public usrname: string = 'shea') {
 		this.name = name
 	}
+
+	getColor() {
+		console.log(this.color)
+	}
+
+	setType(type: string) {
+		this.type = type
+		console.log(this.type)
+	}
 }
 
-console.log(new Person('poat', 'jurd'))
+const poat = new Person('Shea', 'Poat')
+console.log(poat.name, poat.usrname)
+poat.getColor()
+poat.setType('Puppy')
