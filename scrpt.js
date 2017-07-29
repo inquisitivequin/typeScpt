40d14ca729b3622338f41f5ef14c88ec9ba08dde
Jerd <inquisitivequin@gmail.com>
2017-07-28 18:21:36 -0700

    fini

--- scrpt.js
+++ scrpt.js
@@ -1,8 +1,20 @@
-var t = 'test';
-console.log(t);
-t = "another test";
-console.log(t);
-console.log("Rest & Spread");
-var ar = [1, 2, 3, 4, 4, 423, 4];
-console.log(Math.max.apply(Math, ar));
+var double = function (val) { return val * 2; };
+console.log(double(10));
+var greet = function (name) {
+    if (name === void 0) { name = 'shea'; }
+    if (name === undefined) { }
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+var numbers = [-3, 33, 38, 5];
+console.log.apply(console, numbers);
+var newArray = [55, 20].concat(numbers);
+console.log(newArray);
+var testResults = [3.89, 2.99, 1.38];
+var _a = testResults;
+console.log.apply(console, testResults);
+var scientist = { firstName: "Will", experience: 12 };
+var firstName = scientist.firstName, experience = scientist.experience;
+console.log(firstName, experience);
 //# sourceMappingURL=scrpt.js.map
--- scrpt.ts
+++ scrpt.ts
@@ -1,13 +1,48 @@
-let t: string = 'test'
+// Exercise 1 - Maybe use an Arrow Function?
+// var double = function(value) {
+//     return value * 2;
+// };
 
-console.log(t)
+let double = (val: number) => val * 2
 
-t = "another test"
+console.log(double(10));
+ 
+// Exercise 2 - If only we could provide some default values...
+var greet = function (name: string = 'shea') {
+    if (name === undefined) {}
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+ 
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+var numbers = [-3, 33, 38, 5];
+//console.log(Math.min.apply(Math, numbers));
+ 
+console.log(...numbers)
 
-console.log(t)
+// Exercise 4 - I have to think about Exercise 3 ...
+var newArray = [55, 20, ...numbers];
+// Array.prototype.push.apply(newArray, numbers);
+console.log(newArray);
 
-console.log("Rest & Spread")
 
-const ar = [1,2,3,4,4,423,4]
+ 
+// Exercise 5 - That's a well-constructed array.
+var testResults = [3.89, 2.99, 1.38];
+// var result1 = testResults[0];
+// var result2 = testResults[1];
+// var result3 = testResults[2];
+// console.log(result1, result2, result3);
 
-console.log(Math.max(...ar))
+let [] = testResults
+
+console.log(...testResults)
+ 
+// Exercise 6 - And a well-constructed object!
+var scientist = {firstName: "Will", experience: 12};
+// var firstName = scientist.firstName;
+// var experience = scientist.experience;
+let {firstName, experience} = scientist
+console.log(firstName, experience);
+
