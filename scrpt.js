d3f6e3b0e73e80e181e0c97de78a9896462a44c4
Jerd <inquisitivequin@gmail.com>
2017-07-30 16:40:38 -0700

    interfaces

--- scrpt.js
+++ scrpt.js
@@ -1,9 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var circ_1 = require("./circ");
-var circ_2 = require("./circ");
-var rect_1 = require("./rect");
-console.log(circ_1.PI);
-console.log(circ_2.calcCirc(34));
-console.log(rect_1.calcRect(12, 12));
 //# sourceMappingURL=scrpt.js.map
--- scrpt.ts
+++
@@ -1,7 +0,0 @@
-import {PI} from './circ'
-import {calcCirc} from './circ'
-import {calcRect} from './rect'
-
-console.log(PI)
-console.log(calcCirc(34))
-console.log(calcRect(12,12))
