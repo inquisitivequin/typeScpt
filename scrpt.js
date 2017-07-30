8470805484431d415b2bcc6627ad704933d3a88d
Jerd <inquisitivequin@gmail.com>
2017-07-30 15:10:38 -0700

    nameSpace

--- scrpt.js
+++ scrpt.js
@@ -1,1 +1,8 @@
+var PI = 3.14;
+function calcCirc(dia) {
+    return dia * PI;
+}
+function calcRect(width, length) {
+    return width * length;
+}
 //# sourceMappingURL=scrpt.js.map
---
+++ scrpt.ts
@@ -0,0 +1,9 @@
+const PI = 3.14
+
+function calcCirc(dia: number) {
+	return dia * PI
+}
+
+function calcRect(width: number, length: number) {
+	return width * length
+}
